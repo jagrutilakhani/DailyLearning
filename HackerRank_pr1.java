351fb7366779f1e4ebb902b45171facc901405c6
jagrutilakhani <110833531+jagrutilakhani@users.noreply.github.com>
2024-11-09 01:21:50 +0100

    Update HackerRank_pr1.java

--- HackerRank_pr1.java
+++ HackerRank_pr1.java
@@ -1,1 +1,11 @@
 // test hacker rank program 
+
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
