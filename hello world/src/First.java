375e7599118664e4d2b263cb661889c3993c8462
Jagruti Hirpara <jagrutilakhani1989@gmail.com>
2025-01-20 23:03:53 +0100

    Update First.java

--- hello world/src/First.java
+++ hello world/src/First.java
@@ -3,6 +3,6 @@ public class First {
     public static void main(String[] sk){
         System.out.println("hello, world");
         System.out.println("hello, everyone");
-        System.out.print("hi, jaykishan");
+        System.out.print("hi, Check data");
     }
 }
